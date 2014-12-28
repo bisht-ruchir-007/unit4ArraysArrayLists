49de8a0ac5ba44a5c6823ab14dcb975c59ad2901
glin1 <samlin203@yahoo.com>
2014-12-27 17:54:36 -0800

    Update RadarTester.java

--- SignalsInNoiseLab/samlin _radorLab/RadarTester.java
+++ SignalsInNoiseLab/samlin _radorLab/RadarTester.java
@@ -2,21 +2,37 @@ import java.util.ArrayList;
 /**
  * Write a description of class RadarTester here.
  * 
- * @author Sam Lin 
- * @version 12-21-2014
+ * @author @Sam Lin 
+ * @version 12-27-2014
  */
 public class RadarTester
 {
     public static void main(String [] args)
     {
-        Radar checking = new Radar(100,100,2,2,0,0);
+        // rows of the grid, col of the grid, dy , dx 
+        // original row and col 
+        Radar checking = new Radar(100,100,1,2,0,0);
         
-        checking.setNoiseFraction(.01);
-        for (int i =0; i<10; i++)
+        // set the nosie fraction to .005
+        checking.setNoiseFraction(.005);
+        for (int i =0; i<20; i++)
         {
             checking.scan();
         }
-        checking.findTheSecret();
+        checking.getDx_Dy();
+        
+        // rows of the grid, col of the grid, dy , dx 
+        // original row and col 
+        Radar checking2 = new Radar(100,100,5,3,0,0);
+        
+        // set the nosie fraction to .005
+        checking2.setNoiseFraction(.005);
+        for (int i =0; i<20; i++)
+        {
+            checking2.scan();
+        }
+        checking2.getDx_Dy();
+        
     }
     
 }
